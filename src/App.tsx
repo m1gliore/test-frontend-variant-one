4cbf16e314bb08e274f22b9e4a2da0e6f8a92878
Lenovo <raintruwalker@gmail.com>
2023-07-04 15:44:06 +0300

    добавлена маршрутизация для поиска

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/search/:name' element={<Search/>}/>
             </Routes>
         </Router>
     )
