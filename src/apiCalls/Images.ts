059890696d192a6878ec1b9c30787dcbc0206e8e
Lenovo <raintruwalker@gmail.com>
2023-07-04 15:21:39 +0300

    добавлени функции fetchImages

--- src/apiCalls/Images.ts
+++ src/apiCalls/Images.ts
@@ -1,36 +1,53 @@
-import {createClient, ErrorResponse, Photos} from 'pexels';
-import {ImageData} from '../types/ImageData'
+import {createClient} from 'pexels';
+import {ImageData} from '../types/ImageData';
 
-const client = createClient('563492ad6f917000010000014640aabb4e9d420cbe1c0df7daf4c2bf')
+const client = createClient('563492ad6f917000010000014640aabb4e9d420cbe1c0df7daf4c2bf');
 
 export const fetchRandomImage = async (): Promise<ImageData | null> => {
     try {
-        const page = Math.floor(Math.random() * 80) + 1
-        const response: Photos | ErrorResponse = await client.photos.curated({
-            page,
-            per_page: 80
-        })
+        const page = Math.floor(Math.random() * 80) + 1;
+        const response = await client.photos.curated({page, per_page: 80});
         if ('photos' in response) {
             const photo = response.photos[0];
             const imageUrl = photo?.src?.landscape;
             const photographer = photo?.photographer;
             const photographerUrl = photo?.photographer_url;
+
             if (imageUrl && photographer && photographerUrl) {
                 return {
                     imageUrl,
                     photographer,
                     photographerUrl,
-                }
+                };
             } else {
-                console.log('Недостаточно данных для создания изображения')
-                return null
+                console.log('Недостаточно данных для создания изображения');
+                return null;
             }
         } else {
-            console.log('Ошибка при получении изображения:', response)
-            return null
+            console.log('Ошибка при получении изображения:', response);
+            return null;
         }
     } catch (error) {
-        console.log('Ошибка при получении изображения:', error)
-        return null
+        console.log('Ошибка при получении изображения:', error);
+        return null;
     }
-}
+};
+
+export const fetchImages = async (page: number) => {
+    try {
+        const response = await client.photos.curated({page, per_page: 6});
+        if ('photos' in response) {
+            return response.photos.map((photo) => ({
+                imageUrl: photo.src.original,
+                photographer: photo.photographer,
+                photographerUrl: photo.photographer_url,
+            }));
+        } else {
+            console.log('Ошибка при получении изображений:', response);
+            return [];
+        }
+    } catch (error) {
+        console.error('Ошибка при получении изображений:', error);
+        return [];
+    }
+};
